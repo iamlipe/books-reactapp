f80ed70e1b0703e8223a6717ae37cf4b04000b21
Felipe Lima <felipelima@ioasys.com.br>
2022-08-23 11:55:27 -0300

    feat wrapper redux app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Text} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
-import {theme} from '@/styles';
+import {theme} from './styles';
+import {Provider} from 'react-redux';
+import {store} from './store';
 
 const App = () => (
-  <ThemeProvider theme={theme}>
-    <Text>Hello world!</Text>
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <Text>Hello world!</Text>
+    </ThemeProvider>
+  </Provider>
 );
 
 export default App;
