cc78c8f256cf5597ddb6ec2ade44595290068dd3
Felipe Lima <felipelima@ioasys.com.br>
2022-08-23 12:19:36 -0300

    feat wrapper i18next app

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import {ThemeProvider} from 'styled-components/native';
 import {theme} from './styles';
 import {Provider} from 'react-redux';
 import {store} from './store';
+import {I18nextProvider} from 'react-i18next';
+import i18n from './config/i18n';
 
 const App = () => (
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <Text>Hello world!</Text>
+      <I18nextProvider i18n={i18n}>
+        <Text>Hello world!</Text>
+      </I18nextProvider>
     </ThemeProvider>
   </Provider>
 );
