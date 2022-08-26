4617204017e60c2551dac034b3d3748f45e5d81c
Felipe Lima <felipelima@ioasys.com.br>
2022-08-26 13:59:37 -0300

    feat config modulenamemapper setupfiles and ignorepatterns

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,20 @@ const config = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/__mocks__/svgMock.ts',
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/ios',
+    '<rootDir>/android',
+    '<rootDir>/__tests__/jest.setup.js',
+    '<rootDir>/node_modules',
+  ],
   verbose: true,
+  setupFiles: ['<rootDir>/__tests__/jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@?react-navigation||@react-native-community)',
+  ],
 };
 
 module.exports = config;
