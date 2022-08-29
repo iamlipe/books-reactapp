3a7ec0ddf271ad4bee39f72072affda159358b82
Felipe Lima <felipelima@ioasys.com.br>
2022-08-29 14:21:18 -0300

    feat mock axios

--- __tests__/jest.setup.js
+++ __tests__/jest.setup.js
@@ -1,13 +1,31 @@
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('axios', () => {
+  return {
+    create: jest.fn(() => ({
+      interceptors: {
+        request: {use: jest.fn(() => Promise.resolve({data: {foo: 'bar'}}))},
+        response: {use: jest.fn(() => Promise.resolve({data: {foo: 'bar'}}))},
+      },
+    })),
+  };
+});
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
 jest.mock('react-native-localize', () => {
   return {
     findBestAvailableLanguage: jest.fn(() => ({languageTag: 'pt'})),
   };
 });
 
-jest.mock('react-hook-form', () => ({
-  useController: () => ({
-    field: {
-      onChange: jest.fn(),
-    },
-  }),
-}));
+jest.mock('react-hook-form', () => {
+  const originalModule = jest.requireActual('react-hook-form');
+
+  return {
+    __esModule: true,
+    ...originalModule,
+  };
+});
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
